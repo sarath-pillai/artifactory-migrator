bc9e205e31f362fd7becda1e13269e3584e097b6
Sarath Pillai <sarath@slashroot.in>
2025-04-08 15:09:51 +0530

    Fixing formatting

--- main.go
+++ main.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 
-    "nuget-migrator/internal/azure"
-    "nuget-migrator/internal/github"
+	"nuget-migrator/internal/azure"
+	"nuget-migrator/internal/github"
 )
 
 func main() {
-    if len(os.Args) != 2 {
-        fmt.Println("Usage: nuget-migrator <azure_feed_url>")
-        os.Exit(1)
-    }
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: nuget-migrator <azure_feed_url>")
+		os.Exit(1)
+	}
 
-    feedUrl := os.Args[1]
-    pkgs := azure.FetchPackages(feedUrl)
+	feedUrl := os.Args[1]
+	pkgs := azure.FetchPackages(feedUrl)
 
-    for _, pkg := range pkgs {
-        fmt.Printf("📦 %s\n", pkg.Name)
-        for _, version := range pkg.Versions {
-            fmt.Printf("  └─ %s\n", version)
-            file := azure.DownloadPackage(feedUrl, pkg.Name, version)
-            github.PushToGitHub(file)
-        }
-    }
+	for _, pkg := range pkgs {
+		fmt.Printf("📦 %s\n", pkg.Name)
+		for _, version := range pkg.Versions {
+			fmt.Printf("  └─ %s\n", version)
+			file := azure.DownloadPackage(feedUrl, pkg.Name, version)
+			github.PushToGitHub(file)
+		}
+	}
 }
